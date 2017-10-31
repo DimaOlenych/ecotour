9247473cac2a86d30abcfe244d2db7a0c0c8e342
Andriy Andruhovski <andriy.andruhovski@asposeptyltd.com>
2017-10-31 15:45:40 +0200

    Day 06
    Day 06

---
+++ routes/index.js
@@ -0,0 +1,68 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Головна сторінка' });
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'Про фірму' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Контакти' });
+});
+
+router.post("/contact", function(req, res) {
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.message);
+    if (req.body.human === "5") {
+        res.render("contact", { title: 'Контакти' });
+    } else {
+        res.render("error", {
+            title: 'Помилка',
+            message: "Помилка",
+            error: {
+                stack: "Є підозра, що ви не людина"
+            }
+        });
+    }
+});
+
+router.get("/prices", function(req, res) {
+    var tours = [{
+            country_en: "Egypt",
+            country_uk: "Єгипет",
+            days: 6,
+            price: 12000
+        },
+        {
+            country_en: "Greece",
+            country_uk: "Греція",
+            days: 8,
+            price: 12000
+        },
+        {
+            country_en: "Bulgaria",
+            country_uk: "Болгарія",
+            days: 10,
+            price: 14000
+        },
+        {
+            country_en: "Greece",
+            country_uk: "Греція1",
+            days: 9,
+            price: 12000
+        },
+        {
+            country_en: "Bulgaria",
+            country_uk: "Болгарія1",
+            days: 12,
+            price: 14000
+        }
+    ];
+    res.render("prices", { title: "Ціни на тури", tours: tours });
+});
+module.exports = router;
