9d6a21ad004df6266bb5579cbe111e9359f8dd58
Black_Yuzia <osmolyar@ukr.net>
2017-11-22 19:58:35 +0200

    Fix Table Sort | add custom style

--- public/javascripts/admin/tours.js
+++ public/javascripts/admin/tours.js
@@ -1,3 +1,17 @@
 $(function() {
-    $("#tourstable").tablesorter();
+    $("#tourstable").tablesorter({
+        headers: {
+            // assign the secound column (we start counting zero) 
+            3: {
+                // disable it by setting the property sorter to false 
+                sorter: false
+            },
+            // assign the third column (we start counting zero) 
+            4: {
+                // disable it by setting the property sorter to false 
+                sorter: false
+            }
+        }
+    });
+
 });
