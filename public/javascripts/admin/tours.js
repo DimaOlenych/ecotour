35203c99a6403e5c48d0007d507cf822cb1becc1
Black_Yuzia <osmolyar@ukr.net>
2017-11-27 18:44:12 +0200

    fix table

--- public/javascripts/admin/tours.js
+++ public/javascripts/admin/tours.js
@@ -2,12 +2,12 @@ $(function() {
     $("#tourstable").tablesorter({
         headers: {
             // assign the secound column (we start counting zero) 
-            3: {
+            4: {
                 // disable it by setting the property sorter to false 
                 sorter: false
             },
             // assign the third column (we start counting zero) 
-            4: {
+            5: {
                 // disable it by setting the property sorter to false 
                 sorter: false
             }
