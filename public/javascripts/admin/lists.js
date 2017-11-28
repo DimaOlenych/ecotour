fd20e84a93ebb93afa9672305ae773f48600f099
Black_Yuzia <osmolyar@ukr.net>
2017-11-28 17:04:03 +0200

    small fixes + added styles

--- public/javascripts/admin/lists.js
+++ public/javascripts/admin/lists.js
@@ -2,12 +2,12 @@ $(function() {
     $("#liststable").tablesorter({
         headers: {
             // assign the secound column (we start counting zero) 
-            4: {
+            5: {
                 // disable it by setting the property sorter to false 
                 sorter: false
             },
             // assign the third column (we start counting zero) 
-            5: {
+            6: {
                 // disable it by setting the property sorter to false 
                 sorter: false
             }
