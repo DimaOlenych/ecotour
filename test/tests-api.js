38fc1b7cdb7976927adae8e7b0974d1d2e6cc020
Black_Yuzia <osmolyar@ukr.net>
2017-12-04 16:13:20 +0200

    Try create some tests for Lists (failed)

--- test/tests-api.js
+++ test/tests-api.js
@@ -28,13 +28,13 @@ suite('API Tests', function() {
             });
         });
 
-    test('Проверка наличия в списке страницы с названием about',
-        function(done) {
-            rest.get(base + '/page/5a14763acc03e0076c8faa99').on('success', function(data) {
-                assert(data.name === 'about');
-                done();
-            });
-        });
+    // test('Проверка наличия в списке страницы с названием about',
+    //     function(done) {
+    //         rest.get(base + '/page/5a14763acc03e0076c8faa99').on('success', function(data) {
+    //             assert(data.name === 'about');
+    //             done();
+    //         });
+    //     });
 
     test('Проверка возможности получения кода ошибки',
         function(done) {
@@ -105,4 +105,69 @@ suite('Робота з валютою', function() {
                 });
             done();
         });
+});
+
+suite('Робота з СП', function() {
+    let id = "";
+    test('Проверка тайм-аута', function(done) {
+        rest.get(base + '/lists', { timeout: 10000 }).on('timeout', function(ms) {
+            console.log('did not return within ' + ms + ' ms');
+            assert(false);
+        }).on('complete', function(data) {
+            //console.log('Did not time out!');
+            assert(true);
+        });
+        done();
+    });
+    test('Проверка возможности создания СП',
+        function(done) {
+            rest.post(base + '/lists/', {
+                data: { company: "test", representative: "test", adress: "test", valid: "test" },
+            }).on('complete', function(data, response) {
+                assert(response.statusCode == 200);
+                assert(data.company === "test");
+                assert(data.representative == "test");
+                assert(data.adress == "test");
+                assert(data.valid == "test");
+                id = data._id;
+            });
+            done();
+        });
+
+    test('Проверка возможности корректного чтения СП',
+        function(done) {
+            rest.get(base + '/lists/' + id).on('success',
+                function(data, response) {
+                    assert(response.statusCode == 200);
+                    assert(data.company === "test");
+                    assert(data.representative == "test");
+                    assert(data.adress == "test");
+                    assert(data.valid == "test");
+                });
+            done();
+        });
+
+    test('Проверка возможности корректного обновления СП',
+        function(done) {
+            rest.put(base + '/lists/' + id, {
+                data: { company: "EtoTest" },
+            }).on('complete', function(data, response) {
+                assert(response.statusCode == 200);
+                assert(response.statusCode == 200);
+                assert(data.company === "test");
+                assert(data.representative == "test");
+                assert(data.adress == "test");
+                assert(data.valid == "test");
+            });
+            done();
+        });
+
+    // test('Проверка возможности корректного удаления СП',
+    //     function(done) {
+    //         rest.del(base + '/lists/' + id)
+    //             .on('complete', function(data, response) {
+    //                 assert(response.statusCode == 200);
+    //             });
+    //         done();
+    //     });
 });
